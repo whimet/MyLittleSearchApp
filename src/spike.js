9b8a92a55636102335c976c33cb4c22c67c9b106
Yanhui Li <yanhui.li@fabricgroup.com.au>
2019-05-14 21:25:36 +1000

    Deleted spike.js

--- src/spike.js
+++
@@ -1,9 +0,0 @@
-const loki = require('lokijs');
-const usersData = require('../data/users');
-
-const db = new loki('example.db');
-const users = db.addCollection('users');
-
-users.insert(usersData);
-
-console.log(db.getCollection('users').find({tags: {'$contains': 'Shrewsbury'}}));
